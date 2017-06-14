a1051293da54593b199ff267ab866156a0f9104d
lakshman <lakshmanraob@gmail.com>
2017-06-14 12:14:15 +0530

    Sample update

--- facemoji-master/sample.py
+++ facemoji-master/sample.py
@@ -5,9 +5,11 @@ import time
 import cv2
 
 # face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-face_cascade = cv2.CascadeClassifier('faces.xml')
-eyes_cascade = cv2.CascadeClassifier('eyes.xml')
-nose_cascade = cv2.CascadeClassifier('nose.xml')
+# face_cascade = cv2.CascadeClassifier('faces.xml')
+# eyes_cascade = cv2.CascadeClassifier('eyes.xml')
+# nose_cascade = cv2.CascadeClassifier('nose.xml')
+
+faceCascade = cv2.CascadeClassifier('models/haarcascade_frontalface_default.xml')
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
@@ -28,23 +30,23 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # gray
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(30, 30))
-    print "Found {0} face".format(len(faces))
+    faces = faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(30, 30))
+    print("Found {0} face".format(len(faces)))
 
     for (x, y, w, h) in faces:
         image = cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 1)
-        roi_gray = gray[y:y+h,x:x+w]
-        roi_color = image[y:y+h,x:x+w]
-        eyes = eyes_cascade.detectMultiScale(roi_gray)
-        nose = nose_cascade.detectMultiScale(roi_gray)
+        # roi_gray = gray[y:y+h,x:x+w]
+        # roi_color = image[y:y+h,x:x+w]
+        # eyes = eyes_cascade.detectMultiScale(roi_gray)
+        # nose = nose_cascade.detectMultiScale(roi_gray)
         # if nose:
         #     print "Nose found"
         # if eyes:
         #     print "Eyes found"
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,255),1)
-        for (nx,ny,nw,nh) in nose:
-            cv2.rectangle(roi_color,(nx,ny),(nx+nw,ny+nh),(0,255,255),1)
+        # for (ex,ey,ew,eh) in eyes:
+        #     cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,255),1)
+        # for (nx,ny,nw,nh) in nose:
+        #     cv2.rectangle(roi_color,(nx,ny),(nx+nw,ny+nh),(0,255,255),1)
 
     # show the frame
     cv2.imshow("Frame", image)
